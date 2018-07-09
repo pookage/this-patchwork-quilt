73907e0147dbf6269308e1879b05dda70dab1754
pookage <pookagehayes@gmail.com>
2018-07-09 15:34:36 +0100

    switched to sandbox if there's a sandbox param

--- client/index.js
+++ client/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { getQueryParams } from "Utils/utils.js";
 import App from './components/App/App.jsx';
+import Sandbox from "Components/Sandbox/Sandbox.jsx";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const {
+	sandbox = false
+} = getQueryParams();
+
+if(sandbox){
+	ReactDOM.render(<Sandbox />, document.getElementById('root'));
+} else {
+	ReactDOM.render(<App />, document.getElementById('root'));
+}
